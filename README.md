3e3bca7bce8e67941a3f08b9c9c24a64862af6e8
Marco Schlicht <marcoschlicht@onlinehome.de>
2017-12-20 21:07:27 +0100

    added sequence and class diagram

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Intelligente-Task-Verwaltung
 
 Dies ist ein Projekt zur intelligenten Verteilung und Verwaltung von Tasks auf verschiedenen Rechnern. Das Projekt findet im Rahmen des SWE Projektes des Dualen Studiengans "Matse" an der FH-Aachen statt.
+
+## Dependencies
+- Python3
+- Django
+	- `pip install django`
+- MongoDB
---
+++ src/main.py
@@ -0,0 +1,29 @@
+a = "hallo"
+
+
+def swap(s):
+    """TODO: Docstring for swap.
+    :returns: TODO
+
+    """
+    return a[::-1]
+
+
+class MyClass(object):
+
+    """Docstring for MyClass. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        self.wert = 5
+        self.x = 2
+
+    def echo(self, b):
+        """TODO: Docstring for echo.
+
+        :b: TODO
+        :returns: TODO
+
+        """
+        print("hallo"+self.wert+self.x+b)
+
