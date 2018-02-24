be0675fca932be33044dad6c9815b0b41a32aa75
Marco Schlicht <marcoschlicht@onlinehome.de>
2018-02-24 04:04:54 +0100

    startet adding webserver module

--- src/Webserver/master.py
+++ src/Webserver/master.py
@@ -4,6 +4,8 @@ import json
 from mongo_items import Task
 from mongo_items import ServerSettings
 from mongo_items import Server
+from webserver import Webserver
+from interfaces import ITaskManager
 
 
 class MongoConnector(object):
@@ -28,19 +30,39 @@ class ServerTask(object):
         self.task = task
 
 
-class TaskManager(object):
-    """Manages all Tasks."""
+class Master(object):
+    """
+    Manages the server.
 
-    def __init__(self):
+    Contains a task manager, the config, the webserver, the watcher and the executor
+    """
+
+    def __init__(self, task_manager: ITaskManager=None):
         """Initial function"""
+        self.task_manager = task_manager
         self.loadConfig()
-        self.loadTasks()
+        self.loadWebserver()
 
     def loadConfig(self):
         """Loads the first server settings"""
         configs_j = json.loads(MongoConnector.load_server_settings())
         if len(configs_j) >= 1:
             self.config = ServerSettings(configs_j[0])
+
+    def loadWebserver(self):
+        """Loads the webserver"""
+        self.webserver = Webserver(self)
+
+    def startWebserver(self):
+        """37;14M37;14M"""
+        self.webserver.start()
+
+
+class TaskManager(ITaskManager):
+    def __init__(self, master: Master=None):
+        """Initial function"""
+        self.master = master
+        self.loadTasks()
 
     def loadTasks(self):
         """Loads all tasks"""
@@ -51,9 +73,17 @@ class TaskManager(object):
             s_task = ServerTask(task)
             self.tasks.append(s_task)
 
-tm = TaskManager()
+    def execute(task: Task):
+        """ """
+        pass
+
+
+task_man = TaskManager()
+master = Master(task_man)
+task_man.master = master
 print("All tasks:")
-for task in tm.tasks:
+for task in task_man.tasks:
     print(task.task.dict())
 print("Settings:")
-print(tm.config.dict())
+print(master.config.dict())
+master.startWebserver()
---
+++ src/Webserver/webserver.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import redirect, url_for, request, render_template
+from interfaces import ITaskManager
+
+
+class Webserver(object):
+    """
+    TaskManger Webserver
+
+    Can access functions on the task_manager, for Example to execute tasks or add new one.
+    """
+
+    def routes(self):
+        @self.app.route('/')
+        def index():
+            return render_template('index.html')
+        @self.app.route('/hello/<string:name>')
+        def hello_world(name: str):
+            return render_template('hello.html', name=name)
+
+    def __init__(self, task_manager: ITaskManager, host: str="0.0.0.0", port: int=5000, debug: bool=False):
+        """Initial function"""
+        self.task_manager = task_manager
+        self.host = host
+        self.port = port
+        self.debug = debug
+        self.app = Flask(__name__)
+
+    def start(self):
+        self.routes()
+        self.app.run(self.host, self.port, self.debug)
+
+
